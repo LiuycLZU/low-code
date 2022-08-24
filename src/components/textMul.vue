6fc0716b95b0e87c1a5fc9daa50dea3a29815e9a
LiuycLZU <105481879+LiuycLZU@users.noreply.github.com>
2022-08-24 18:40:41 +0800

    Create textMul.vue

---
+++ src/components/textMul.vue
@@ -0,0 +1,12 @@
+<template>
+	<textarea :placeholder="placeholder"></textarea>
+</template>
+<script>
+	export default {
+	data() {
+		return {
+			placeholder:'请输入内容...\n这是一个多行文本框'
+		}
+	}
+}
+</script>
